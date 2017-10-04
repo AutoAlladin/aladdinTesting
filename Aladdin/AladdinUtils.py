8961525c3e4aa9edff2d41cc1c04d3f3eb1bdb10
Developer2 Mikhail Shkodenko <dev2@aps-trade.com>
2017-10-04 15:52:16 +0300

    add AladdinUtils class

---
+++ Aladdin/AladdinUtils.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class WebTestSession():
+    def __init__(self):
+        self.drv = webdriver.Chrome()
+        #chrm = webdriver.Chrome(chrm)
+        self.drv.maximize_window()
+        self.drv.implicitly_wait(5)
+
+    def click_reg_btn(self):
+        self.drv.get('https://alltenders.ald.in.ua/uk')
+        btn_registration = self.drv.find_element_by_xpath(".//*[@id='navbarCollapse']/div[2]/div[1]/a[2]")
+        btn_registration.click()
+        WebDriverWait(self.drv, 15).until(
+            EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "РЕЄСТРАЦІЯ ПІДПРИЄМСТВА"))
+
+    def input_text_field(self,_id,val):
+        try:
+            WebDriverWait(self.drv, 1).until(EC.visibility_of_element_located((By.ID, _id)))
+        except:
+            self.click_reg_btn()
+
+        text_field = self.drv.find_element_by_id(_id)
+        text_field.send_keys(val)
+        self.drv.get_screenshot_as_file("company_name.png")
+        return text_field.get_attribute('value')
+
+    def close(self):
+        self.drv.close()
+        pass
--- Aladdin/Authorization.py
+++ Aladdin/Authorization.py
@@ -1,49 +1,36 @@
 import unittest
 
 import time
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def set_web_driver():
-    chrm = webdriver.Chrome()
-    #chrm = webdriver.Chrome(chrm)
-    chrm.maximize_window()
-    chrm.implicitly_wait(5)
-    return chrm
+from Aladdin.AladdinUtils import *
 
-def click_reg_btn(хром):
-    хром.get('https://alltenders.ald.in.ua/uk')
-    btn_registration = хром.find_element_by_xpath(".//*[@id='navbarCollapse']/div[2]/div[1]/a[2]")
-    btn_registration.click()
-    WebDriverWait(хром, 15).until(
-        EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "РЕЄСТРАЦІЯ ПІДПРИЄМСТВА"))
 
 class OpenMainPage(unittest.TestCase):
-    хром_для_всего_тэста=None
+    wts=None
     def setUp(self):
-        self.хром_для_всего_тэста = set_web_driver()
+        self.wts = WebTestSession()
 
     def tearDown(self):
-        self.хром_для_всего_тэста.close()
+        self.wts.close()
 
-    @unittest.skip("qqq")
+    @unittest.skip("test_open_page - Не представляет интереса на даный момент")
     def test_open_page(self):
         try:
-            self.хром_для_всего_тэста.get('https://alltenders.ald.in.ua/uk')
-            WebDriverWait(self.хром_для_всего_тэста, 5).until(
+            self.wst.drv.get('https://alltenders.ald.in.ua/uk')
+            WebDriverWait(self.wst.drv, 5).until(
                 EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "РЕЄСТРАЦІЯ"))
             self.assertTrue(True)
         except Exception as e:
             self.assertTrue(False, 'внятное слово '+e.__str__())
 
-
 class OpenRegistrationPage(OpenMainPage):
-    @unittest.skip("Не представляет интереса на даный момент")
+    @unittest.skip("test_open_registration_page - Не представляет интереса на даный момент")
     def test_open_registration_page(self):
         try:
-            click_reg_btn(хром=self.хром_для_всего_тэста)
+            self.wts.click_reg_btn()
             self.assertTrue(True)
         except Exception as e:
             self.assertTrue(False, 'Ошибка открытия формы регистрации\n'+e.__str__())
@@ -52,23 +39,10 @@ class FullFillPage(OpenRegistrationPage):
     def test_company_name(self):
         try:
             input_val_company_name = "Тестовое название компании"
-            click_reg_btn(хром=self.хром_для_всего_тэста)
-            company_name = self.хром_для_всего_тэста.find_element_by_id("nameUA")
-            company_name.send_keys(input_val_company_name)
-            self.хром_для_всего_тэста.get_screenshot_as_file("company_name.png")
-
-            result_val_company_name = company_name.get_attribute('value')
-            self.assertEqual(input_val_company_name, result_val_company_name, "Не совпадают исходные даные и то что оказалось в поле браузера")
+            self.assertEqual(
+                input_val_company_name,
+                self.wts.input_text_field("nameUA",input_val_company_name) ,
+                "Не совпадают исходные даные и то что оказалось в поле браузера")
         except Exception as e:
-            self.хром_для_всего_тэста.get_screenshot_as_file("company_name_ERROR.png")
+            self.wts.drv.get_screenshot_as_file("company_name_ERROR.png")
             self.assertTrue(False, "Ошибка при вводе названия компании\n"+e.__str__())
-
-    def test_company_name_en(self):
-        try:
-            input_val_company_name_en = "Test name jf company"
-            company_name_en = self.хром_для_всего_тэста.find_element_by_id("nameEN")
-            company_name_en.send_keys(input_val_company_name_en)
-            self.хром_для_всего_тэста.get_screenshot_as_file("company_name_en.png")
-            result_val_company_name_en = company_name_en.
-
-
