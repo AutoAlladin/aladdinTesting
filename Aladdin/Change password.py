b99148bf6866ea728e647425941023300dfd3082
autoalladin <qa@aps-trade.com>
2017-10-20 09:23:31 +0300

    add changes pswd

---
+++ Aladdin/Change password.py
@@ -0,0 +1,19 @@
+import unittest
+from selenium import webdriver
+from Aladdin import Authorization
+
+
+
+
+
+
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
