ed5425d54c61c9882964532f5214fa928a5c2794
Developer2 Mikhail Shkodenko <dev2@aps-trade.com>
2017-10-23 16:47:56 +0300

    add   docs test example

--- Aladdin/Authorization/Login.py
+++ Aladdin/Authorization/Login.py
@@ -13,84 +13,42 @@ from Aladdin.Registration.OpenMainPage import OpenMainPage
 publicWST = None;
 def setUpModule():
     global publicWST
-    publicWST = WebTestSession()
+    publicWST = WebTestSession( 'https://identity.ald.in.ua/Account/Login')
 
 
 def tearDownModule():
     publicWST.drv.close()
 
-#
-# def field_input(_id, val):
-#     field_input=browser.drv.find_element_by_id("_id")
-#     field_input.send_keys("val")
-#     return field_input.get_attribute('value')
-#
-# def text_input(_id, val, input_val=None):
-#     try:
-#         if input_val is None:
-#             input_val = field_input(_id, val)
-#         browser.drv.assertEqual(
-#             input_val,
-#             browser.drv.field_input
-#         )
-#     except Exception as e:
-#         browser.assertTrue(False, "Ошибка при вводе текста\n" + e.__str__())
-
-
-
-#
-#
-# class openChrome(unittest.TestCase):
-#     def __init__(self):
-#         try:
-#             self.drv = webdriver.Chrome()
-#             self.drv.maximize_window()
-#             self.drv.implicitly_wait(10)
-#             #self.drv.get('https://192.168.80.169:44310/Account/Login')
-#             self.drv.get('https://identity.ald.in.ua/Account/Login')
-#             self.assertTrue(True)
-#         except Exception as e:
-#             self.assertTrue(False, 'Не открывается форма логина\n' + e.__str__())
-#
-#
-
-
 
 class Login(unittest.TestCase):
-    wts = None
+
     query = {"input_val": None, "q": {"name": "Login", "version": "0.0.0.1"}}
 
     @classmethod
     def setUpClass(cls):
         cls.wts = publicWST
-        cls.wts.url = 'https://identity.ald.in.ua/Account/Login'
 
-    @classmethod
+
     def test_01_email(self):
-        #text_input("exampleInputEmail1", "envarra@gmail.com")
-        # email = browser.drv.find_element_by_id("exampleInputEmail1")
-        # email.send_keys("envarra@gmail.com")
-        test_input(self, "exampleInputEmail1", q=self.query)
+        test_input(self, "exampleInputEmail1", **self.query)
 
-    @classmethod
     def test_02_pswd(self):
-        # pswd = browser.drv.find_element_by_id("pswd")
-        # pswd.send_keys("qwerty1234")
-        # time.sleep(10)
-        test_input(self, q=self.query)
+        test_input(self,"pswd", **self.query)
 
-    @classmethod
     def test_03_btn(self):
         btn_sub = self.wts.drv.find_element_by_id("submitLogin")
         btn_sub.click()
-        #WebDriverWait(browser.drv, 10).until(EC._find_element(By.XPATH), "html/body")
         time.sleep(10)
 
-class EditInfo(OpenMainPage):
+class EditInfo(unittest.TestCase):
     def test_01_go_to_user_profile(self):
+        WebDriverWait(self.wts.drv, 10).until(
+            EC.visibility_of_element_located((By.ID, "link_about")))
+
         user_prof = self.wts.drv.find_element_by_id("link_about")
-        time.sleep(5)
         user_prof.click()
+        WebDriverWait(self.wts.drv, 20).until(
+            EC.element_to_be_clickable((By.ID, "btnSaveUser")))
 
     def test_02_click_tab_company(self):
 
