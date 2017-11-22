2c64e2c7651fec0bfb0044d4aa4608a48331d912
Developer2 Mikhail Shkodenko <dev2@aps-trade.com>
2017-11-22 09:46:37 +0200

    refactoring CreateAccount

--- Aladdin/Billing/CreateAccount.py
+++ Aladdin/Billing/CreateAccount.py
@@ -52,7 +52,7 @@ class CreateAccount(unittest.TestCase):
     def setUpClass(cls):
         cls.in_dic = dict()
         with open(os.path.dirname(os.path.abspath(__file__)) + '\\input.json', 'r',
-                encoding="UTF-8") as test_params_file:
+                  encoding="UTF-8") as test_params_file:
             cls.in_dic = json.load(test_params_file)
 
         cls.in_dic["new_account"]["uuid"] = str(uuid.uuid4())
@@ -61,21 +61,18 @@ class CreateAccount(unittest.TestCase):
         cls.old_edr = cls.in_dic["new_account"]["edrpou"]
 
 
-
     def test_01_new_UUID_new_EDR(self):
-        res = new_account(que='need_create_company_account',
-                    str_json=self.msg_create_company_account,
-                    in_dic=self.in_dic)
-
         self.start_test_method = datetime.datetime.now()
-        self.final_test_method = datetime.datetime.now()
-        print("duration", (self.final_test_method - self.start_test_method).total_seconds())
+        res = new_account(que='need_create_company_account',
+                          str_json=self.msg_create_company_account,
+                          in_dic=self.in_dic)
 
-
+        print("duration", (datetime.datetime.now() - self.start_test_method).total_seconds())
         self.assertEqual(res[0:4], "PASS")
 
 
     def test_02_new_UUID_old_EDR(self):
+        self.start_test_method = datetime.datetime.now()
         self.in_dic["new_account"]["uuid"] = str(uuid.uuid4())
         self.in_dic["new_account"]["edrpou"] = self.old_edr
         self.msg_create_company_account = json.dumps({'companyAccount': self.in_dic["new_account"]})
@@ -84,15 +81,12 @@ class CreateAccount(unittest.TestCase):
                     str_json=self.msg_create_company_account,
                     in_dic=self.in_dic)
 
-        self.start_test_method = datetime.datetime.now()
-        self.final_test_method = datetime.datetime.now()
-        print("duration", (self.final_test_method - self.start_test_method).total_seconds())
-
-
-        self.assertEqual(res[0:4], "PASS")
+        print("duration", (datetime.datetime.now() - self.start_test_method).total_seconds())
+        self.assertEqual(res[0:6], "FAILED")
 
 
     def test_03_old_UUID_old_EDR(self):
+        self.start_test_method = datetime.datetime.now()
         self.in_dic["new_account"]["uuid"] = self.old_id
         self.in_dic["new_account"]["edrpou"]=self.old_edr
         self.msg_create_company_account = json.dumps({'companyAccount': self.in_dic["new_account"]})
@@ -100,56 +94,46 @@ class CreateAccount(unittest.TestCase):
         res = new_account(que='need_create_company_account',
                     str_json=self.msg_create_company_account,
                     in_dic=self.in_dic)
-        self.start_test_method = datetime.datetime.now()
-        self.final_test_method = datetime.datetime.now()
-        print("duration", (self.final_test_method - self.start_test_method).total_seconds())
 
-
+        print("duration", (datetime.datetime.now() - self.start_test_method).total_seconds())
         self.assertEqual(res[0:4], "PASS")
 
+
     def test_04_fail_UUID_new_EDR(self):
+        self.start_test_method = datetime.datetime.now()
         self.in_dic["new_account"]["uuid"] = '000000000'
         self.in_dic["new_account"]["edrpou"]=self.old_edr
-
         self.msg_create_company_account = json.dumps({'companyAccount': self.in_dic["new_account"]})
-
 
         res = new_account(que='need_create_company_account',
                     str_json=self.msg_create_company_account,
                     in_dic=self.in_dic)
-        self.start_test_method = datetime.datetime.now()
-        self.final_test_method = datetime.datetime.now()
-        print("duration", (self.final_test_method - self.start_test_method).total_seconds())
 
-
+        print("duration", (datetime.datetime.now() - self.start_test_method).total_seconds())
         self.assertEqual(res[0:4], "PASS")
 
     def test_05_new_UUID_less_EDR(self):
+        start_test_method = datetime.datetime.now()
         self.in_dic["new_account"]["uuid"] = str(uuid.uuid4())
         self.msg_create_company_account = json.dumps({'companyAccount': {"uuid": self.old_id, "edrpou": "123456"}})
 
         res = new_account(que='need_create_company_account',
-                    str_json=self.msg_create_company_account,
-                    in_dic=self.in_dic)
-        self.start_test_method = datetime.datetime.now()
-        self.final_test_method = datetime.datetime.now()
-        print("duration", (self.final_test_method - self.start_test_method).total_seconds())
+                          str_json=self.msg_create_company_account,
+                          in_dic=self.in_dic)
 
-
-        TestCase.assertEqual(res[0:4], "PASS")
+        print("duration", (datetime.datetime.now() - start_test_method).total_seconds())
+        self.assertEqual(res[0:4], "PASS")
 
     def test_06_new_UUID_more_EDR(self):
+        start_test_method = datetime.datetime.now()
         self.in_dic["new_account"]["uuid"] = str(uuid.uuid4())
         self.msg_create_company_account = json.dumps({'companyAccount': {"uuid": self.old_id, "edrpou": "123456789012345678"}})
 
         res = new_account(que='need_create_company_account',
                     str_json=self.msg_create_company_account,
                     in_dic=self.in_dic)
-        self.start_test_method = datetime.datetime.now()
-        self.final_test_method = datetime.datetime.now()
-        print("duration", (self.final_test_method - self.start_test_method).total_seconds())
 
-
+        print("duration", ( datetime.datetime.now() - start_test_method).total_seconds())
         self.assertEqual(res[0:4], "PASS")
 
 
---
+++ Aladdin/Billing/dicts.py
@@ -0,0 +1,65 @@
+atl=[{
+		"tenderId": 13581,
+		"lotId": 11,
+		"amount": 22318.24,
+		"currency": "UAH",
+		"descriptions": "Тендер: Лента транспортерная Лот №1 Позиції: Лента транспортерная CNB-6EB-A1 (B=800mm, L=1800mm) бесконечная; Лента транспортёрная CNB 6TB-A1 B=1100 L=2700 бесконечная",
+		"totalMoney": 44.63648,
+		"companyUuid": "8f7e760f-6e40-43b6-846b-dc37b98cb807",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}, {
+		"tenderId": 25004,
+		"lotId": 11,
+		"amount": 17300.0,
+		"currency": "UAH",
+		"descriptions": "Тендер: Многофункциональный газовый мультиблок Лот №1 Позиції: Многофункциональный газовый мультиблок Kromschroder CG15R03D2W5WZZ",
+		"totalMoney": 34.6,
+		"companyUuid": "b35b858f-2e8a-4c2e-bfad-4a56e4ac6a91",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}, {
+		"tenderId": 25027,
+		"lotId": 11,
+		"amount": 26000.0,
+		"currency": "UAH",
+		"descriptions": "Тендер: Частотный преобразователь Danfoss Лот №1 Позиції: Частотный преобразователь Danfoss VLT FC 51 18 kW",
+		"totalMoney": 52.0,
+		"companyUuid": "7d13fb24-97f6-48cc-949b-59cf3e4e51df",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}, {
+		"tenderId": 25054,
+		"lotId": 11,
+		"amount": 44215.0,
+		"currency": "UAH",
+		"descriptions": "Тендер: Мука рисовая Лот №1 Позиції: Мука рисовая",
+		"totalMoney": 88.43,
+		"companyUuid": "d169bf77-c911-47fb-95ee-6e2da37a8697",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}, {
+		"tenderId": 25072,
+		"lotId": 11,
+		"amount": 575763.38,
+		"currency": "UAH",
+		"descriptions": "Тендер: Шкафы металлические для одежды Лот №1 Позиції: Шкаф металический ячеистый для одежды 3-секционный (900х500х1800) с полочкой в каждом отделении (фото в прикрепленном файле); Шкаф металический для одежды односекционный (300х500х1800) фото в прикрепленном файле",
+		"totalMoney": 1151.52676,
+		"companyUuid": "f10fc014-5a40-4e97-b310-bbf442c9fb0b",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}, {
+		"tenderId": 25203,
+		"lotId": 11,
+		"amount": 138000.0,
+		"currency": "UAH",
+		"descriptions": "Тендер: Прицепные шины Лот №1 Позиції: Шина Bridgestone 385/65-22,5 R164 (шт)",
+		"totalMoney": 276.0,
+		"companyUuid": "0a341661-0e56-4f83-bff1-320a27387c05",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}, {
+		"tenderId": 25242,
+		"lotId": 11,
+		"amount": 117000.0,
+		"currency": "UAH",
+		"descriptions": "Тендер: Сыроватка сухая молочная(подсырная) Лот №1 Позиції: Сыровотка сухая молочная (подсырная)",
+		"totalMoney": 234.0,
+		"companyUuid": "3e074ee7-8f4a-41b7-8ce1-1a562abd8f30",
+		"serviceIdentifierUuid": "5a4b7a1b-76c2-4208-a49e-712c117cb1f5"
+	}
+]
