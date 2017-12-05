424cec0a556572672c1118be25a6dbf7300129f4
Autoaladdin <qa@aps-trade.com>
2017-12-05 13:45:05 +0200

    user company form locators added full

--- Prozorro/Pages/UserCompanyForm.py
+++ Prozorro/Pages/UserCompanyForm.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 
 
 class UserCompanyForm:
@@ -24,6 +25,10 @@ class UserCompanyForm:
         self.emails1_added = self.drv.find_element_by_id("emails1")
         self.delete_email_1_btn = self.drv.find_element_by_id("delete_email_1")
         self.select_countries = self.drv.find_element_by_id("select_countries")
+        self.select_regions = self.drv.find_element_by_id("select_regions")
+        self.select_cities = self.drv.find_element_by_id("select_cities")
+        self.street = self.drv.find_element_by_id("street")
+        self.post_code = self.drv.find_element_by_id("post_code")
         self.bankName_0 = self.drv.find_element_by_id("bankName_0")
         self.mfo_0 = self.drv.find_element_by_id("mfo_0")
         self.bankAccount_0 = self.drv.find_element_by_id("bankAccount_0")
@@ -35,6 +40,21 @@ class UserCompanyForm:
         self.is_vat = self.drv.find_element_by_id("is_vat")
         self.checkAgreementPolicy = self.drv.find_element_by_id("checkAgreementPolicy")
         self.save_changes_btn = self.drv.find_element_by_id("save_changes")
+
+    def companyForm(self, dic, company):
+        self.full_name.send_keys(company["subj_legal_name"])
+        self.full_name_en.send_keys(company["subj_legal_name_eng"])
+        self.short_name.send_keys(company["subj_short_name"])
+        self.short_name_en.send_keys(company["subj_short_name_eng"])
+        self.bankName_0.send_keys(company["bank_name"])
+        self.mfo_0.send_keys(company["mfo"])
+        self.bankAccount_0.send_keys(company["account"])
+        self.street.send_keys(company["addr_street"])
+        self.post_code.send_keys(company["addr_post_code"])
+        Select(select_type).select_by_label("Ownership")
+
+
+
 
 
 
