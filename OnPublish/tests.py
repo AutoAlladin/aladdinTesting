dfba29a00b7e0035e04084d9035b08aaaba18820
Developer2 Mikhail Shkodenko <dev2@aps-trade.com>
2018-01-17 09:38:49 +0200

    aladdin loader

--- OnPublish/tests.py
+++ OnPublish/tests.py
@@ -2,12 +2,9 @@ import unittest
 import sys
 from optparse import make_option, OptionParser
 
-from copy import deepcopy
-
 import xmlrunner
 
 from Aladdin.Accounting.AladdinUtils import WebTestSession, AvaliableBrowsers
-from Aladdin.Accounting.Authorization.Login import Login
 from Aladdin.Accounting.decorators.StoreTestResult import create_result_DB
 from Aladdin.Billing.CreateAccount import *
 from OnPublish.MainPage.load_main_page import Load_main_page
@@ -34,26 +31,17 @@ def s_load_main_page(g, t, cmbro):
     suite.addTest(Load_main_page("menu_presented", _params=qqq))
     return suite
 
-
-
-
-
-if __name__ == '__main__':
-
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    subdirs= [x[0] for x in os.walk(os.path.dirname(os.path.abspath(__file__))) if not x[0].startswith("_") and not x[0].startswith(".")]
-    sys.path.extend(subdirs)
-
+def runner(arg):
     parser = OptionParser()
     parser.add_option("-s", action="store", type="string")
     parser.add_option("-g", action="store", type="string")
     parser.add_option("-b", action="store", type="string")
 
-    (options, args) = parser.parse_args()
+    (options, args) = parser.parse_args(arg)
 
-    #runner = unittest.TextTestRunner(verbosity=2)
+    # runner = unittest.TextTestRunner(verbosity=2)
     runner = xmlrunner.XMLTestRunner(output='test-reports',
-                                         verbosity=2)
+                                     verbosity=2)
     opt = options.s
     bro = options.b
 
@@ -67,8 +55,7 @@ if __name__ == '__main__':
     ttt = None
 
     if opt == 'main_page':
-        ttt = s_load_main_page(options.g,"xxx",bro)
-
+        ttt = s_load_main_page(options.g, "xxx", bro)
 
     if ttt is not None:
         try:
@@ -82,3 +69,9 @@ if __name__ == '__main__':
                 ttt.suite_params["DB"].test_result.update(
                     {"_id": ttt.suite_params["result_id"]},
                     {"$set": {"test_result": "PASSED"}})
+
+
+
+
+if __name__ =='__main__':
+    runner(None)
