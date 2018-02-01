b58bd3a1cd87d033857de6b1bc9a6989a4e1a32c
Developer2 Mikhail Shkodenko <dev2@aps-trade.com>
2018-02-01 16:49:54 +0200

    below згидшір ntcnc

--- OnPublish/Procedures/below.py
+++ OnPublish/Procedures/below.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -12,26 +13,9 @@ from Prozorro.Utils import waitFadeIn
 
 
 class Test_Below(ParamsTestCase):
-    """
-    self.drv.find_element_by_xpath("").click()
-    return TenderNew(self.drv). \
-        set_description(dic, nom). \
-        set_curr(). \
-        set_multilot(dic, is_multilot). \
-        set_dates(dic). \
-        click_next_button(). \
-        add_lot(lots, dic). \
-        add_item(dic, lots, items). \
-        click_next_button(). \
-        add_features(dic, lots, features). \
-        add_doc(docs). \
-        click_finish_edit_button(). \
-        click_publish_button()
-    """
 
     @add_res_to_DB(test_name="Меню создания тендеров")
     def create_menu(self):
-        # TODO push to parameter
         self.wts.drv.get('https://test-gov.ald.in.ua/Account/Login')
 
         with self.subTest("авторизация"):
@@ -66,26 +50,29 @@ class Test_Below(ParamsTestCase):
                 with self.subTest("создать процедуру - "+value.text):
                     self.assertIn(value.text, proc_list, "Невалидный текст меню создания тендера - "+value.text)
 
-    @add_res_to_DB(test_name="Выбор создания допорогового")
-    def select_below_menu(self):
-        # TODO push to parameter
-        self.wts.drv.get('https://test-gov.ald.in.ua/purchases')
+    def select_below_menu_F(self):
+        self.wts.drv.get(self.parent_suite.suite_params["tender_json"]["main"]["url"])
         waitFadeIn(self.wts.drv)
         WebDriverWait(self.wts.drv, 10).until(
             expected_conditions.visibility_of_element_located(
-                (By.ID, "btn_create_purchase")))\
-        .click()
+                (By.ID, "btn_create_purchase"))) \
+            .click()
+
+        waitFadeIn(self.wts.drv)
         with self.subTest("пункт меню создать Below"):
             WebDriverWait(self.wts.drv, 10).until(
                 expected_conditions.visibility_of_element_located(
-                    (By.XPATH, "//a[@href='/Purchase/Create/BelowThreshold']")))\
-            .click()
+                    (By.XPATH, "//a[@href='/Purchase/Create/BelowThreshold']"))) \
+                .click()
 
-            WebDriverWait(self.wts.drv, 10).until(
-                expected_conditions.visibility_of_element_located(
-                    (By.ID, "next_step")))
+        WebDriverWait(self.wts.drv, 10).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "next_step")))
 
-            self.parent_suite.suite_params.update({"tender_new": TenderNew(self.wts.drv) })
+    @add_res_to_DB(test_name="Выбор создания допорогового")
+    def select_below_menu(self):
+        self.select_below_menu_F()
+        self.parent_suite.suite_params.update({"tender_new": TenderNew(self.wts.drv) })
 
     @add_res_to_DB(test_name="Допороговый - тексты")
     def set_description(self):
@@ -105,7 +92,7 @@ class Test_Below(ParamsTestCase):
     def set_multilot(self):
         dic = self.parent_suite.suite_params["tender_json"]
         tn = self.parent_suite.suite_params["tender_new"]
-        tn = tn.set_multilot(dic, True)
+        tn = tn.set_multilot(dic, "true")
         self.parent_suite.suite_params.update({"tender_new": tn})
 
     @add_res_to_DB(test_name="Допороговый - периоды")
@@ -117,7 +104,7 @@ class Test_Below(ParamsTestCase):
 
         WebDriverWait(self.wts.drv, 20).until(
             expected_conditions.visibility_of_element_located(
-                (By.ID, "add_procurement_subject0")))
+                (By.ID, "buttonAddNewLot")))
 
         div = self.wts.drv.find_element_by_xpath("//div[@ng-controller='endEditPurchaseController']")
         txt=div.get_attribute("data-ng-init")[15:-1]
@@ -126,9 +113,136 @@ class Test_Below(ParamsTestCase):
         self.parent_suite.suite_params.update({"tender_new": tn})
         self.parent_suite.suite_params.update({"tenderID": model["purchase"]["purchaseId"]})
 
+    @add_res_to_DB(test_name="Допороговый - + лот")
+    def add_lot(self):
+        dic = self.parent_suite.suite_params["tender_json"]
+        tn = self.parent_suite.suite_params["tender_new"]
+        tn = tn.add_lot(1, dic)
+        self.parent_suite.suite_params.update({"tender_new": tn})
 
-    def create_simple_below(self):
-        pass
+    @add_res_to_DB(test_name="Допороговый - + позиция")
+    def add_item(self):
+        dic = self.parent_suite.suite_params["tender_json"]
+        tn = self.parent_suite.suite_params["tender_new"]
+        tn = tn.add_item(dic, 1,1)
+        self.parent_suite.suite_params.update({"tender_new": tn})
+
+    @add_res_to_DB(test_name="Допороговый - + позиция")
+    def add_item(self):
+        dic = self.parent_suite.suite_params["tender_json"]
+        tn = self.parent_suite.suite_params["tender_new"]
+        tn = tn.add_item(dic, 1, 1).click_next_button()
+        self.parent_suite.suite_params.update({"tender_new": tn})
+
+    @add_res_to_DB(test_name="Допороговый - + параметр")
+    def add_features(self):
+        dic = self.parent_suite.suite_params["tender_json"]
+        tn = self.parent_suite.suite_params["tender_new"]
+        tn = tn.add_features(dic, 1, 1,2)
+        self.parent_suite.suite_params.update({"tender_new": tn})
+
+    @add_res_to_DB(test_name="Допороговый - + документ")
+    def add_doc(self):
+        dic = self.parent_suite.suite_params["tender_json"]
+        tn = self.parent_suite.suite_params["tender_new"]
+        tn = tn.add_doc(1)
+        self.parent_suite.suite_params.update({"tender_new": tn})
+
+    def open_draft_by_url_F(self):
+        with self.subTest("авторизация"):
+            self.wts.drv.get('https://test-gov.ald.in.ua/Account/Login')
+            LoginPage(self.wts.drv).login(
+                self.parent_suite.suite_params["authorization"]["owner_login"],
+                self.parent_suite.suite_params["authorization"]["owner_password"]
+            )
+
+        with self.subTest("открыть тендер"):
+            id = self.parent_suite.suite_params["tenderID"]
+            url = self.parent_suite.suite_params["tender_json"]["main"]["url"][:-1]
+            url = url + "/" + str(id)
+            self.wts.drv.get(url)
+
+            WebDriverWait(self.wts.drv, 15).until(
+                expected_conditions.visibility_of_element_located(
+                    (By.ID, "goToListPurchase")))
+
+    @add_res_to_DB(test_name="Допороговый - открыть тендер по урлу")
+    def open_draft_by_url(self):
+        self.open_draft_by_url_F()
+
+    @add_res_to_DB(test_name="Допороговый - открыть тендер по урлу - изменить описание")
+    def open_draft_by_url_edit(self):
+        self.open_draft_by_url_F()
+
+        waitFadeIn(self.wts.drv)
+        WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "purchaseEdit")))\
+        .click()
+        save = WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "save_changes")))
+        description = WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "description")))
+        description.send_keys("отрекдактировано")
+        waitFadeIn(self.wts.drv)
+        save.click()
+        waitFadeIn(self.wts.drv)
+        WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "movePurchaseView")))\
+        .click()
+        waitFadeIn(self.wts.drv)
+        WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "goToListPurchase")))
+
+    @add_res_to_DB(test_name="Допороговый - открыть тендер по урлу - удалить")
+    def open_draft_by_url_delete(self):
+        waitFadeIn(self.wts.drv)
+        WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "deletePurchase")))\
+        .click()
+
+        waitFadeIn(self.wts.drv)
+        WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.XPATH, "//div[@class='jconfirm-buttons']/button[1]")))\
+        .click()
+
+        waitFadeIn(self.wts.drv)
+        WebDriverWait(self.wts.drv, 15).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID,"hrefPurchases")))
+
+    @add_res_to_DB(test_name="Допороговый полный опубликовать")
+    def create_below_publish(self):
+        self.select_below_menu_F()
+        dic = self.parent_suite.suite_params["tender_json"]
+
+        TenderNew(self.wts.drv). \
+            set_description(dic, "XXX"). \
+            set_curr(). \
+            set_multilot(dic, "true"). \
+            set_dates(dic). \
+            click_next_button(). \
+            add_lot(1, dic). \
+            add_item(dic, 1, 1). \
+            click_next_button(). \
+            add_features(dic, 1, 2). \
+            add_doc(1). \
+            click_finish_edit_button(). \
+            click_publish_button()
+
+        tenderGUD = WebDriverWait(self.wts.drv, 25).until(
+            expected_conditions.visibility_of_element_located(
+                (By.ID, "purchaseGuid"))).text
+
+        self.parent_suite.suite_params.update({"tenderGUID": tenderGUD})
+        self.log(tenderGUD)
+
 
 
 
