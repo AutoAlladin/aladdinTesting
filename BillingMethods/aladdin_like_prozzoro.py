194e6a46b1afa83ed7717e587ef1c5919f6cfdbb
autoalladin <qa@aps-trade.com>
2018-02-16 15:46:41 +0200

    update url

--- BillingMethods/aladdin_like_prozzoro.py
+++ BillingMethods/aladdin_like_prozzoro.py
@@ -11,20 +11,20 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     @add_res_to_DB(test_name='get_balance_positive')
     def test_01_get_balance_positive(self):
         par = self.parent_suite.suite_params["par"]["test_01"]
-        req = requests.get("http://192.168.95.153:91/api/balance", params=par)
+        req = requests.get(par["url"], params=par)
         self.assertEqual(req.status_code, 200, "Метод balance не отработал")
 
     @add_res_to_DB(test_name="get_balance_acc_negativ")
     def test_02_get_balance_acc_negative(self):
         par = self.parent_suite.suite_params["par"]["test_02"]
-        req = requests.get("http://192.168.95.153:91/api/balance", params=par)
+        req = requests.get(par["url"], params=par)
         self.assertNotEquals(req.status_code, 200, 201)
         self.assertEqual(req.status_code, 400, "Метод balance отработал с несуществующим гуидом. Ожидаемый статус-код - 400.")
 
     @add_res_to_DB(test_name="get_balance_without_guid_negative")
     def test_03_get_balance_without_guid_negative(self):
         par = self.parent_suite.suite_params["par"]["test_03"]
-        req = requests.get("http://192.168.95.153:91/api/balance", params=par)
+        req = requests.get(par["url"], params=par)
         self.assertNotEquals(req.status_code, 200, 201)
         self.assertEqual(req.status_code, 400, "Метод balance отработал без гуида. Ожидаемый статус-код - 400.")
 
@@ -32,21 +32,21 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     def test_04_reserve_balance_positive(self):
         par = self.parent_suite.suite_params["par"]["test_04"]
         #par["siteType"] = self.params["siteType"]
-        req = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertEqual(req.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
 
     @add_res_to_DB(test_name="reserve_balance_tender_id_is_null_negative")
     def test_05_reserve_balance_tender_id_is_null_negative(self):
         par = self.parent_suite.suite_params["par"]["test_05"]
-        req = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par), headers={"content-type": "application/json"})
+        req = requests.post(par["url"], data=json.dumps(par), headers={"content-type": "application/json"})
         self.assertNotEquals(req.status_code, 200, 201)
         self.assertEqual(req.status_code, 400, "Метод Reserve отработал с нулевым тендером. Ожидаемый статус-код - 400.")
 
     @add_res_to_DB(test_name="reserve_balance_total_money_is_zero_positive")
     def test_06_reserve_balance_total_money_is_zero_positive(self):
         par = self.parent_suite.suite_params["par"]["test_06"]
-        req = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertEqual(req.status_code, 200,
                          "Метод Reserve не отработал. Не прошло резервирование с нулевым балансом.")
@@ -55,31 +55,31 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     def test_07_return_monies_positive(self):
         # резервирование
         par = self.parent_suite.suite_params["par"]["test_07_1"]
-        req_rez = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req_rez = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertEqual(req_rez.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
 
         par = self.parent_suite.suite_params["par"]["test_07"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMonies", data=json.dumps(par), headers= {"content-type": "application/json"})
+        req = requests.post(par["url"], data=json.dumps(par), headers= {"content-type": "application/json"})
         self.assertEqual(req.status_code, 200, "Метод ReturnMonies не отработал. Средства не вернулись.")
 
     @add_res_to_DB(test_name="return_monies_without_reserve")
     #@unittest.skip("Без предварительного резерва отвечает 200, BUG 5603")
     def test_08_return_monies_without_reserve_negative(self):
         par = self.parent_suite.suite_params["par"]["test_08"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMonies", data=json.dumps(par), headers= {"content-type": "application/json"})
+        req = requests.post(par["url"], data=json.dumps(par), headers= {"content-type": "application/json"})
         self.assertEqual(req.status_code, 400, "Метод ReturnMonies не отработал. Средства не вернулись.")
 
     @add_res_to_DB(test_name="return_monies_tender_is_null_negative")
     def test_09_return_monies_tender_is_null_negative(self):
         # резервирование
         par = self.parent_suite.suite_params["par"]["test_09_1"]
-        req_rez = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req_rez = requests.post(par["url"], data=json.dumps(par),
                                 headers={"content-type": "application/json"})
         self.assertEqual(req_rez.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
         #Возврат
         par = self.parent_suite.suite_params["par"]["test_09"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMonies", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertNotEquals(req.status_code, 200, 201)
         self.assertEqual(req.status_code, 400, "Метод ReturnMonies отработал с нулевым тендером. Ожидаемый статус-код - 400.")
@@ -88,13 +88,13 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     def test_10_return_monies_error_negative(self):  #передача json без lotId, tenderId
         #резервирование
         par = self.parent_suite.suite_params["par"]["test_10_1"]
-        req_rez = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req_rez = requests.post(par["url"], data=json.dumps(par),
                                 headers={"content-type": "application/json"})
         self.assertEqual(req_rez.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
 
         #возврат средств
         par = self.parent_suite.suite_params["par"]["test_10"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMonies", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertNotEquals(req.status_code, 200)
         self.assertEqual(req.status_code, 400, "Метод ReturnMonies отработал без параметра \"tenderId\". Ожидаемый статус-код - 400")
@@ -105,19 +105,19 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     def test_11_return_monies_by_company_uuid_positive(self):
         #резервирование
         par = self.parent_suite.suite_params["par"]["test_11_1"]
-        req_rez = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req_rez = requests.post(par["url"], data=json.dumps(par),
                                 headers={"content-type": "application/json"})
         self.assertEqual(req_rez.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
 
         #списание
         par = self.parent_suite.suite_params["par"]["test_11_2"]
-        req_wr = requests.post("http://192.168.95.153:91/api/balance/WriteOffMoney", data=json.dumps(par),
+        req_wr = requests.post(par["url"], data=json.dumps(par),
                                headers={"content-type": "application/json"})
         self.assertEqual(req_wr.status_code, 200, "Метод WriteOffMoney не отработал. Средства не списались")
 
         #возврат после списания
         par = self.parent_suite.suite_params["par"]["test_11"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMoniesByCompany", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertEqual(req.status_code, 200, "Метод WriteOffMoney не отработал. Средства не вернулись после списания")
 
@@ -125,12 +125,12 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     def test_12_return_monies_by_company_uuid_tender_is_null_negative(self):
         # резервирование
         par = self.parent_suite.suite_params["par"]["test_12_1"]
-        req_rez = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req_rez = requests.post(par["url"], data=json.dumps(par),
                                 headers={"content-type": "application/json"})
         self.assertEqual(req_rez.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
         #возврат средств после списания
         par = self.parent_suite.suite_params["par"]["test_12"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMoniesByCompany", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertNotEquals(req.status_code, 200, 201)
         self.assertEqual(req.status_code, 400)
@@ -138,7 +138,7 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     @add_res_to_DB(test_name="return_monies_by_company_uuid_error_negative")
     def test_13_return_monies_by_company_uuid_error_negative(self):
         par = self.parent_suite.suite_params["par"]["test_13"]
-        req = requests.post("http://192.168.95.153:91/api/balance/ReturnMoniesByCompany", data=json.dumps(par),
+        req = requests.post(par["url"], data=json.dumps(par),
                             headers={"content-type": "application/json"})
         self.assertNotEquals(req.status_code, 200, 201)
         self.assertEqual(req.status_code, 400)
@@ -148,7 +148,7 @@ class TestAladdinLikeProzorro(ParamsTestCase):
     def test_14_write_off_money_positive(self):
         # резервирование
         par = self.parent_suite.suite_params["par"]["test_14_1"]
-        req_rez = requests.post("http://192.168.95.153:91/api/balance/Reserve", data=json.dumps(par),
+        req_rez = requests.post(par["url"], data=json.dumps(par),
                                 headers={"content-type": "application/json"})
         self.assertEqual(req_rez.status_code, 200, "Метод Reserve не отработал. Средства не зарезервировались")
 
